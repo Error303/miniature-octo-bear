6eb9c1fbcaea869d54e18a0572ffd6eeb4913e72
Oskari Lindblad <gimmel@gmail.com>
2013-10-03 14:07:24 +0300

    inittijeah

---
+++ muussarirss.php
@@ -0,0 +1,104 @@
+<?php
+
+$csv = "Aries 300 Music System;ARP 2500;ARP 2600;ARP Avatar;ARP Axxe;ARP Explorer I;ARP Little Brother;ARP Pro-Soloist;ARP Pro/DGX;ARP Soloist;ARP Solus;Arturia MiniBrute;BOSS HC-2 Hand Clapper;BOSS PC-2 Percussion Synthesizer;Buchla 100 Series;Buchla 200 Series;Buchla Music Easel;Chimera Synthesis bC16;Control Synthesis Deep Bass Nine;Crumar Spirit;Dave Smith Instruments Evolver;Dave Smith Instruments Mono Evolver;Dave Smith Instruments Mopho;Dave Smith Instruments Mopho Keyboard;Davoli Davolisint;Doepfer Dark Energy;Doepfer Dark Energy II;Doepfer MS-404;E-mu Modular Systems;EKO EKOsynth P15;Electro Harmonix Mini-Synthesizer;Electronic Dream Plant (EDP) Gnat;Electronic Dream Plant (EDP) Wasp;Elka Soloist 505;EML ElectroComp 200;EML ElectroComp 400/401;EML ElectroComp 500;EML SynKey;EMS Synthi 100;EMS Synthi A;EMS Synthi AKS;EMS Synthi E;EMS VCS3;FBT Electronica Synther-2000;Frostwave Resonator;Future Retro 777;Future Retro Revolution;Future Retro XS;Hammond 102200;Jen Electronics SX-1000 Synthetone;Jen Electronics SX-2000 Synthetone;JoMoX MBase 01;JoMoX MBase 11;Kawai Synthesizer 100F;Kawai Synthesizer 110F;Korg 770;Korg M500 Micro-Preset;Korg MiniKorg-700;Korg MiniKorg-700S;Korg Monotron;Korg Monotron Duo;Korg MS-10;Korg MS-20;Korg MS-20 mini;Korg MS-50;Korg SB-100 Synthe-Bass;Korg Sigma;Korg X-911;Ladyada x0xb0x;MacBeth Studio Systems M3X;MacBeth Studio Systems M5;MAM FreeBass 383;MAM MB 33;MAM MB 33 mkII;MAM VF-11;MAM WARP 9;Metasonix TS-21 HellFire;Metasonix TS-22 Pentode FilterBank;Moog Etherwave Theremin;Moog Liberation;Moog Little Phatty;Moog MF-101 Lowpass Filter;Moog MF-102 Ring Modulator;Moog MF-103 12-Stage Phaser;Moog MF-104 Analog Delay;Moog MF-105M MIDI MuRF;Moog Micromoog;Moog Minimoog;Moog Minimoog Voyager;Moog Minimoog Voyager Electric Blue;Moog Minimoog Voyager Old School;Moog Minimoog Voyager Rack Mount Edition;Moog Minimoog Voyager Select Series;Moog Minimoog Voyager XL;Moog Minitmoog;Moog Multimoog;Moog Prodigy;Moog Rogue;Moog Satellite;Moog Slim Phatty;Moog Source;Moog Synthesizer 10;Moog Synthesizer 12;Moog Synthesizer 15/35/55;Moog Synthesizer 1c/2c/3c;Moog Synthesizer 1p/2p/3p;Moog Taurus I;Moog Taurus II;Moog Vocoder;Novation Bass Station Rack;Novation Super Bass Station;Oberheim OB-1;Oberheim SEM;Octave The Kitten;Oxford Synthesiser Company OSCar;PAiA Fatman;PAiA Proteus 1;Powertran Transcendent 2000;Realistic Concertmate MG-1;Roland CMU-810;Roland GR-500;Roland MC-202;Roland ProMars MRS-2;Roland Saturn 09;Roland SH-09;Roland SH-1;Roland SH-1000;Roland SH-101;Roland SH-2;Roland SH-2000;Roland SH-3a;Roland SH-5;Roland System 100;Roland System 100m;Roland System 700;Roland TB-303;RSF Kobol Expander;RSF Modular Model 11;Sequential Circuits Pro One;Serge Modular;Siel Mono;Spectral Audio Neptune;Spectral Audio Neptune 2;Spectral Audio ProTone;Steiner-Parker Minicon;Steiner-Parker Synthacon;Steiner-Parker SynthaSystem;Studio Electronics ATC-1;Studio Electronics Midimoog;Studio Electronics SE-1;Syntecno TeeBee;Synton Syrinx;Technosaurus Microcon;Teisco S100P;Teisco Synthesizer 100F;Triadex Muse;Vermona Synthesizer;Waldorf MiniWorks 4-Pole;Wersi Bass Synthesizer;Wiard Modular Systems;Will Systems MAB-303;Wurlitzer Orbit III;Yamaha CS-10;Yamaha CS-15;Yamaha CS-15D;Yamaha CS-20m;Yamaha CS-30;Yamaha CS-5;Yamaha CS01;Yamaha CS01 II;Yamaha ED10;Yamaha SY-1;Yamaha SY-2;Yamaha SY20;Akai AX60;Akai AX73;Akai AX80;Akai VX600;Akai VX90;Alesis Andromeda A6;Analogue Systems RS-Integrator;Analogue Systems Sorcerer;ARP Odyssey 1;ARP Odyssey 2;ARP Odyssey 3;ARP Omni Mk 1;ARP Omni Mk 2;ARP Quadra;ARP Quartet;ARP Solina String Ensemble;ARP Solina String Synthesizer;BOSS DR-110 Dr. Rhythm;BOSS DR-55 Dr. Rhythm;Buchla 200e Series;Buchla 400 Series;Buchla 700 Series;Buchla Touché;Casio CT-101;Casio CT-401;Cheetah MS-6;Con Brio ADS 100;Con Brio ADS 200;Con Brio ADS 200-R;Crumar Bit 01;Crumar Bit 99;Crumar Bit One;Crumar DS-2;Crumar Multiman-S;Crumar Orchestrator;Crumar Performer;Crumar Stratus;Dave Smith Instruments Poly Evolver;Dave Smith Instruments Poly Evolver Rack;Dave Smith Instruments Prophet '08;Dave Smith Instruments Tempest;Dave Smith Instruments Tetra;Doepfer A-100;E-mu Audity;Elka EK-22;Elka EM-22;Elka Synthex;EML ElectroComp 100;EML ElectroComp 101;EML Poly-Box;Ensoniq ESQ-1;Ensoniq ESQ-M;Farfisa Soundmaker;Formanta Polivoks;Gleeman Pentaphonic;Horst MWT-01 Midi Wave Theremin;JoMoX AirBase 99;JoMoX SunSyn;JoMoX XBase 09;Kawai SX-210;Kawai SX-240;Korg 900PS Preset Synthesizer;Korg Delta;Korg DSM-1;Korg DSS-1;Korg DW-6000;Korg DW-8000;Korg EX-800;Korg EX-8000;Korg KPR-77;Korg Lambda ES50;Korg Maxi-Korg 800DV;Korg MiniPops 35 (MP-35);Korg Mono/Poly;Korg Poly-61;Korg Poly-61M;Korg Poly-800;Korg Poly-800 mkII;Korg Polyphonic Ensemble P;Korg Polyphonic Ensemble S;Korg PolySix;Korg PS-3100;Korg PS-3200;Korg PS-3300;Korg Rhythm 55 (KR-55);Korg Rhythm 55 (KR-55B);Korg Trident;Korg Trident mkII;Korg VC-10 Vocoder;Lennar Digital Sylenth1;Logan String Melody II;MAM ADX1;Marion Systems MSR-2;Moog Memorymoog;Moog Opus-3;Moog Polymoog 203a;Moog Polymoog Keyboard 280a;Moog Sonic Six;Mutronics MIDI Mutator;Novation D-Station;Novation Drum Station;Novation Drum Station v2;Oberheim Eight Voice;Oberheim Four Voice;Oberheim Matrix 1000;Oberheim Matrix 12;Oberheim Matrix 6;Oberheim Matrix 6R;Oberheim OB-8;Oberheim OB-Mx;Oberheim OB-SX;Oberheim OB-X;Oberheim OB-Xa;Oberheim Two Voice;Oberheim Xpander;Octave The Cat;Octave Voyetra 8;PAiA Stringz ’n’ Thingz;PPG Realizer;Rhodes Chroma;Rhodes Chroma Polaris;Roland Alpha Juno 1;Roland Alpha Juno 2;Roland CMU-800;Roland CR-68;Roland CR-78;Roland CR-8000;Roland GR-700;Roland HS-10;Roland HS-60;Roland Juno-106;Roland Juno-6;Roland Juno-60;Roland Jupiter-4;Roland Jupiter-6;Roland Jupiter-8;Roland JX-10;Roland JX-3P;Roland JX-8P;Roland MKS-10;Roland MKS-30;Roland MKS-50;Roland MKS-7;Roland MKS-70;Roland MKS-80;Roland RS-09;Roland RS-202 Strings;Roland RS-505 Paraphonic;Roland SH-7;Roland SVC-350 Vocoder;Roland TR-606;Roland TR-66;Roland TR-808;Roland TR-909;Roland VP-330 Vocoder Plus;Sequential Circuits DrumTraks;Sequential Circuits Fugue;Sequential Circuits Max;Sequential Circuits MultiTrak;Sequential Circuits Prelude;Sequential Circuits Prophet 10;Sequential Circuits Prophet 5;Sequential Circuits Prophet 600;Sequential Circuits Prophet T8;Sequential Circuits Six-Trak;Sequential Circuits Split-8;Sherman Electronics FilterBank;Sherman Electronics FilterBank 2;Sherman Electronics Quad Modular Filter;Siel Cruise;Siel DK600;Siel DK70;Siel DK700;Siel DK80;Siel EX600;Siel Opera 6;Siel Orchestra;Siel Orchestra 2;SoundMaster Memory Rhythm SR-88;SoundMaster Stix ST-305;Studio Electronics Omega8;Synthesizers.com Portable Systems;Synthesizers.com Rack Mount Systems;Synthesizers.com Studio Systems;Technosaurus Selector;Teisco SX-240;Teisco SX-400;Teisco Synthesizer 110F;Vermona Action Filter (AF-1);Vermona DRM-1 mkII;Vermona Dual Analog Filter (DAF-1);Vermona Mephisto;Vermona Perfourmer;Waldorf Microwave;Waldorf X-Pole;Yamaha CS-40m;Yamaha CS-50;Yamaha CS-60;Yamaha CS-70m;Yamaha CS-80;Yamaha GX-1;Yamaha SK10;Yamaha SK20;Yamaha SK30;Yamaha SK50D;Yamaha SS30";
+$hyvat_synat = str_getcsv($csv, ";");
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'rss_php.php';
+require_once 'approximate-search.phps';
+require_once 'krumo/class.krumo.php';
+
+function strstrbi($haystack, $needle, $before_needle=FALSE, $include_needle=TRUE, $case_sensitive=FALSE) {
+	//Find the position of $needle
+	if($case_sensitive) {
+	$pos=strpos($haystack,$needle);
+	} else {
+	$pos=strpos(strtolower($haystack),strtolower($needle));
+	}
+
+	//If $needle not found, abort
+	if($pos===FALSE) return FALSE;
+
+	//Adjust $pos to include/exclude the needle
+	if($before_needle==$include_needle) $pos+=strlen($needle);
+
+	//get everything from 0 to $pos?
+	if($before_needle) return substr($haystack,0,$pos);
+
+	//otherwise, go from $pos to end
+	return substr($haystack,$pos);
+}
+
+$synafeed = new rss_php;
+$synafeed->load('http://muusikoiden.net/extra/rss/tori.php?category=4');
+$synaitems = $synafeed->getItems(true);
+
+$samblerfeed = new rss_php;
+$samblerfeed->load('http://muusikoiden.net/extra/rss/tori.php?category=23');
+$sambleritems = $samblerfeed->getItems(true);
+
+$levenmatchfactor = 3;
+
+?>
+
+<!DOCTYPE html>
+<html lang="fi">
+<meta http-equiv="Content-Type"
+content="text/html;charset=UTF-8">
+<head>
+  <title>muussarijee</title>
+</head>
+<body>
+	<?php
+
+	foreach ($synaitems as $key => $value) {
+		$string = strstrbi($value['title']['value'], 'Myydään: ', FALSE, FALSE);
+		$string = strstrbi($string, ' (', TRUE, FALSE);
+		$matches = array();
+		$levenmatches = array();
+		$regex = '(' . implode('|', $hyvat_synat) . ')';
+		$num_found = preg_match_all($regex, $string, $matches);
+
+		if ($num_found == 0) {
+			foreach ($hyvat_synat as $hyva_syna) {
+				if (levenshtein(strtolower($string), strtolower($hyva_syna)) <= $levenmatchfactor) {
+					$levenmatches[] = $hyva_syna;
+				}
+			}
+			if (isset($levenmatches[0])) {
+				print 'Oiskohan tää jees? Muistuttaa vähäsen: ' . implode(', ', $levenmatches) . '<br/><a href="'.$value['link']['value'].'">'. $string .'</a><br/>';
+			}			
+			
+		} else {
+			print 'Jee, <a href="'.$value['link']['value'].'">'. $string .'</a> ois myynnis!<br/><br/>';
+		}
+	}
+
+	foreach ($sambleritems as $key => $value) {
+		$string = strstrbi($value['title']['value'], 'Myydään: ', FALSE, FALSE);
+		$string = strstrbi($string, ' (', TRUE, FALSE);
+		$matches = array();
+		$levenmatches = array();
+		$regex = '(' . implode('|', $hyvat_synat) . ')';
+		$num_found = preg_match_all($regex, $string, $matches);
+
+		if ($num_found == 0) {
+			foreach ($hyvat_synat as $hyva_syna) {
+				if (levenshtein(strtolower($string), strtolower($hyva_syna)) <= $levenmatchfactor) {
+					$levenmatches[] = $hyva_syna;
+				}
+			}
+			if (isset($levenmatches[0])) {
+				print 'Oiskohan tää jees? Muistuttaa vähäsen: ' . implode(', ', $levenmatches) . '<br/><a href="'.$value['link']['value'].'">'. $string .'</a><br/>';
+			}			
+			
+		} else {
+			print 'Jee, <a href="'.$value['link']['value'].'">'. $string .'</a> ois myynnis!<br/><br/>';
+		}
+	}
+
+	?>
+</body>
+</html>
